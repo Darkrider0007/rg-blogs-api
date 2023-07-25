c346e3e7e23c4d27459faf6ee310e48dd75e8d01
Rohan Gope <rg2512@it.jgec.ac.in>
2023-07-26 00:24:57 +0530

    test

--- db.js
+++ db.js
@@ -3,7 +3,7 @@
 // export const db = mysql.createConnection({
 //     host: 'localhost',
 //     user: 'root',
-//     password: 'PASSWORD@Rohan#2002',
+//     password: 
 //     database: 'blog'
 // })
 
